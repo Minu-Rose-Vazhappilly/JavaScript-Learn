b744d1cb5436b447e778792df3946c2ad431dfd2
Minu-Rose-Vazhappilly <minurosevazhappilly29@gmail.com>
2025-06-19 17:45:51 +0530

    added hw of 19-06-2025

--- loop.js
+++ loop.js
@@ -16,4 +16,69 @@ for(let i = 0;i<3;i++){
 }
 console.log(numbers);
 console.log(`second largest one is ${numbers[1]}`);
+console.log('-------------------------------------------');
 
+//write a program to print JavaScript 3 times 
+i = 1;
+while(i<=3){
+    console.log("JavaScript");
+    i++;
+}
+console.log('-------------------------------------------');
+//write a program to print numbers from 1 to 5
+i=1;
+while(i<=5){
+    console.log(i);
+    i++;
+    
+}
+console.log('-------------------------------------------');
+//write a program to print numbers from 10 to 1
+j=10;
+while(j>0){
+    console.log(j);
+    j--; 
+}
+console.log('-----------------even numbers from 1--------------------------');
+//write a program to print even numbers between 1 to 50
+console.log('hi');
+
+z=2;
+while(z <= 50){
+    console.log(z);
+    z+=2;   
+}
+console.log('--------------even numbers from zero-----------------------------');
+//another way
+z=0
+while(z <= 50){
+    if(z%2 == 0){
+    console.log(z);
+    z+=2; 
+    }  
+}
+console.log('------------------sum of all numbers-----------------------------');
+
+//w.a.p to print sum of all numbers between 1 to 10
+i=0;
+sum = 0;
+while(i<=10){
+    sum = sum + i;
+    i++;
+}
+console.log(sum);
+console.log('----------------------------------------------------');
+
+//w.a.p
+// 2 3 4
+// 24 369 4936
+
+i = 2;
+number = 12;
+while(i<=4){
+    result = i * number;
+    console.log(result);
+    i++;
+    number = (number * 10)+ i;
+}
+  
