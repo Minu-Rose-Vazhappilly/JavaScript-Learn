e7e0c2ede75677b5738d447e8f85c093c9c2a7b7
Minu-Rose-Vazhappilly <minurosevazhappilly29@gmail.com>
2025-06-22 09:53:58 +0530

    added some basic problems

--- loop.js
+++ loop.js
@@ -81,4 +81,85 @@ while(i<=4){
     i++;
     number = (number * 10)+ i;
 }
+console.log('----------------------------------------------------');
+//another method
+i = 1;
+input = 3;
+str ='';
+while(i<=input){
+    str = str + i
+    i++;
+}
+console.log(input*str);
+
+console.log('--------------another pattern same question as above-------------------');
+
+//2 +22 3+33+333 4+44+444+4444
+i = 1;
+input = 2;
+str = '';
+sum = 0;
+while(i<=input){
+    str = str+input;
+    sum = sum + Number(str);
+    i++;
+}
+console.log(sum);
+console.log('-------------------------------');
+
+//write a program to find the reverse of a given number.
+//num%10 - will give last digit as the remainder
+//num*10=add zero to the once place.
+input = 123;
+str='';
+while(input!=0){
+    remainder = input%10;
+    str=str+remainder;
+    input = Math.floor(input/10);
+}
+console.log(str);
+console.log('-----------another method------------');
+input = 123;
+rev=0;
+while(input!=0){
+    remainder = input%10;
+    rev=rev*10+remainder;
+    input = Math.floor(input/10);
+}
+console.log(str);
+console.log('----------------------------------------');
+
+//w.a.p to print a given number is palindrome or not
+//reverse and original is same = plaindrome.
+original = 1001
+input = original;
+rev=0;
+while(input!=0){
+    remainder = input%10;
+    rev=rev*10+remainder;
+    input = Math.floor(input/10);
+}
+if(original == rev){
+    console.log("palindrome");
+}else{
+    console.log("not palindrome");
+    
+}
+
+//w.a.p to find the factorial of a given number.
+input = 5;
+i=1;
+product = 1;
+if(input == 0){
+    product= 1;
+}else{
+while(i<=input){
+    product = product * i;
+    i++;
+}
+}
+console.log(product);
+//w.a.p to find a given number is armstrong or not.
+//armstrong = 153 = 1*3 + 5*3 + 3*3 = 153....
+
   
