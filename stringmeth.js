84f8b5a06cf26dd6986ec7af7e3e1675451b306a
Minu-Rose-Vazhappilly <minurosevazhappilly29@gmail.com>
2025-07-11 11:05:06 +0530

    string methods examples

---
+++ stringmeth.js
@@ -0,0 +1,43 @@
+x = 'hello';
+yx='HEELLOW'
+console.log(x);
+console.log(x.length);
+console.log(x[2]);
+y=['h','e','l','l','o']
+y[2] = 'a'
+//array is mutable
+console.log(y);
+//string is immutable
+console.log(x.startsWith('h'));
+console.log(x.endsWith('llo'));
+console.log(x.endsWith('llow'));
+console.log(x.substring(2,4));
+console.log(x.toUpperCase());
+console.log(yx.toLowerCase());
+str1 = '        Luminar'
+console.log(str1);
+console.log(str1.trim());
+console.log(x.slice(2))
+console.log(x.slice(-3))
+str = 'hello all'
+s = str.split('o')
+console.log(s);
+s = str.split('e')
+console.log(s);
+s = str.split('')
+console.log(s);
+s2 = Array.from(str)
+console.log(s2);
+text = 'I am working in google'
+text1 = 'I am working in google,but the office of google is faraway'
+console.log(text.replace('google','Microsoft'))
+text = 'Good Morning all'
+console.log(text1.replaceAll('google','Microsoft'))
+
+
+
+
+
+
+
+
