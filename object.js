e31ee23a86e204a5ab50f9032933aae90557ec1d
Minu-Rose-Vazhappilly <minurosevazhappilly29@gmail.com>
2025-07-18 16:01:19 +0530

    modified with objects

--- object.js
+++ object.js
@@ -264,3 +264,116 @@ op = {}
         op[item] = 1
     }
  }
+
+ console.log('----------------');
+ accounts=[
+    {
+        acno:1000,ac_type:'savings',balance:45000,transaction:[
+            {
+                to:1001,amount:5000,msg:'ebill',mode:'gpay'
+            },
+            {
+                to:1002,amount:2000,msg:'emi',mode:'neft'
+            },
+            {
+                to:1003,amount:1000,msg:'recharge',mode:'phonePay'
+            },
+        ]
+    },
+    {
+        acno:1001,ac_type:'current',balance:30000,transaction:[
+            {
+                to:1000,amount:1000,msg:'grossary',mode:'gpay'
+            },
+            {
+                to:1002,amount:7000,msg:'gift',mode:'phonePay'
+            },
+            {
+                to:1003,amount:10000,msg:'emi',mode:'neft'
+            },
+        ]
+    },
+    {
+        acno:1002,ac_type:'fixed',balance:100000,transaction:[
+            {
+                to:1000,amount:5000,msg:'ebill',mode:'gpay'
+            },
+            {
+                to:1001,amount:2000,msg:'emi',mode:'neft'
+            },
+            {
+                to:1003,amount:1000,msg:'recharge',mode:'phonePay'
+            },
+        ]
+    },
+    {
+        acno:1003,ac_type:'savings',balance:30000,transaction:[
+            {
+                to:1001,amount:5000,msg:'ebill',mode:'gpay'
+            },
+            {
+                to:1002,amount:2000,msg:'emi',mode:'neft'
+            },
+            {
+                to:1000,amount:1000,msg:'recharge',mode:'phonePay'
+            },
+        ]
+    }
+]
+
+// 1. total number of accounts
+count = 0
+accounts.forEach(item=>{
+    count++;
+})
+console.log(count);
+
+console.log('another method');
+console.log(`total number of accounts:${accounts.length}`);
+
+
+
+// 2. print account number whose ac_type is savings
+
+accounts.filter((item)=>item.ac_type == 'savings').forEach((item)=>console.log(item.acno))
+
+// 3.print the balance of accnount number 1000
+ accounts.filter((item)=>item.acno == 1000).forEach((item)=>console.log(item.balance));
+
+ bal = accounts.find((item)=>item.acno == 1000).balance
+ console.log(bal);
+ 
+// 4. print all gpay transactions
+ accounts.map((item)=>item.transaction).flat().filter((item)=>item.mode == 'gpay').forEach((item)=>console.log(item));
+ 
+// 5. print all transaction whose amount > 5000
+accounts.map((item)=>item.transaction).flat().filter((item)=>item.amount>5000).forEach((item)=>console.log(item))
+// 6. print credit transaction of account 1002
+console.log('----------------------');
+accounts.map((item)=>item.transaction).flat().filter((item)=>item.to == 1002).forEach((item)=>console.log(item))
+//another method
+credit = accounts.map((item)=>item.transaction).flat().filter((item)=>item.to == 1002)
+// 7. print debit transaction of account 1002
+const debit = accounts.find((item)=>item.acno == 1002).transaction
+console.log(debit);
+
+// 8. print transaction history of 1002
+ const transaction_history = {
+    credit:credit,
+    debit:debit
+ }
+ console.log(transaction_history);
+ //to use as array use spread operators
+transhist=[...credit,...debit]
+console.log(transhist);
+
+// 9. print highest balance account details
+console.log('---------------');
+
+maxbal = accounts.reduce((acc1,acc2)=>acc1.balance>acc2.balance?acc1:acc2)
+console.log(maxbal);
+
+
+
+
+ 
