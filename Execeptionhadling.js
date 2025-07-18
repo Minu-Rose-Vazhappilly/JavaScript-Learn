7ffdab0f01259bd6cf3cb5ab297615624752a786
Minu-Rose-Vazhappilly <minurosevazhappilly29@gmail.com>
2025-07-18 16:06:01 +0530

    Error handling

---
+++ Execeptionhadling.js
@@ -0,0 +1,11 @@
+exp = '*10*2-3'
+//used majorily in serverside 
+try{let result = eval(exp)
+console.log(result);}
+catch{
+    console.log('error due to incorrect expression');
+    
+}
+finally{console.log('execution ended')}
+
+
